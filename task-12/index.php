17ea71c37464d0bc47fc04125949dcd7452f882f
Ali Hamza <hamzamashooq492@gmail.com>
2024-11-05 10:34:38 +0500

    Int & Float & Concatenation

---
+++ task-12/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $value = 24.56764;
+
+    echo "The value is  $value";
+    echo "<br>";
+    echo "the datatype is ";
+    var_dump($value);
+    echo "<br>";
+
+    $value = 2467;
+    echo "The value is  $value";
+    echo "<br>";
+    echo "the datatype is ";
+    var_dump($value);
+
+    ?>
+</body>
+
+</html>
